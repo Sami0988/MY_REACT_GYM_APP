35d39a5089b411deca41765d20f5045439305b7f
samuel <zsami9017@gmail.com>
2025-08-04 03:54:21 -0700

    HERO PAGE WITH LIGHT AND DARK FUCTIONALITY

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,44 @@
-import React, { use, useEffect } from "react";
-import Navbar from "./Navbar/Navbar";
 import { useTheme } from "../Theme/Themecontext";
+import background from "../assets/Hero.png";
+import mainImage from "../assets/main.png";
 
 const Hero = () => {
   const { theme, toggleTheme } = useTheme();
 
-  return <div className="">This is hereo section</div>;
+  return (
+    <div
+      className="relative w-full h-screen bg-cover bg-center brightness-75"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      {/* Gradient overlay */}
+      <div className="absolute inset-0 bg-gradient-to-t from-[#332F64] to-transparent z-10"></div>
+
+      {/* Content */}
+
+      <div className="relative z-20 flex flex-col-reverse md:flex-row md:justify-around items-center justify-center px-4 md:px-16 gap-6 md:gap-12">
+        <div className="flex flex-col space-y-6">
+          <div className="text-4xl md:text-6xl font-bold text-white leading-tight">
+            <span className="block">Start a better</span>
+            <span className="block">shape of you!</span>
+            <span className="block text-[#FFFF7D]">Come Join Us!</span>
+          </div>
+          <div>
+            <button className="bg-[#FFFADF] text-black hover:bg-amber-100 px-6 py-3 rounded-full transition-all duration-200">
+              Learn More
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <img
+            src={mainImage}
+            alt="Main Image"
+            className="w-[300px] md:w-[500px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,34 +12,38 @@ import {
 } from "react-icons/fa";
 import { IoMenu } from "react-icons/io5";
 import { useTheme } from "../../Theme/Themecontext";
+import logo from "../../assets/Logo.svg";
 
 const Navbar = () => {
   const [isMeanuOpen, setIsMeanuOpen] = useState(false);
   const { theme, toggleTheme } = useTheme();
-  
+  useEffect(() => {
+    console.log("Theme changed to:", theme);
+  }, [theme]); // Log theme changes for debugging
 
   return (
-    <div className="flex justify-between items-center text-gray-800 py-2 px-5 md:px-32">
+    <div className="flex justify-between items-center text-gray-800 dark:bg-slate-900 dark:text-white h-15 px-5 md:px-20 shadow-md bg-[#f2f5fa]">
       {/* Logo */}
-      <div className="flex items-center text-[#1A1363]">
-        <FaDumbbell className="text-4xl" />
-        <div className="flex flex-col pl-2 leading-tight font-bold">
-          <p className="text-lg">SAMUEL</p>
-          <span className="text-blue-500 text-xl">FITNESS</span>
+      <div className="flex items-start ">
+        <img src={logo} alt="logo" className="w-[150px] h-[100px] pt-4" />
+
+        <div className="hidden sm:flex flex-col justify-center sm:-ml-10 mt-6 text-[#1A1363] dark:text-white">
+          <span className="text-xl font-bold">STAMINA</span>
+          <span className="text-xl font-bold">FITNESS</span>
         </div>
       </div>
 
       {/* Menu Items */}
-      <ul className="hidden xl:flex items-center gap-12 font-semibold text-base">
+      <ul className="hidden xl:flex items-center gap-12 font-semibold text-base pl-5">
         {NavLink.map((item, index) => (
           <li
             key={index}
-            className="p-3 hover:bg-sky-400 hover:text-white rounded-md transition-all cursor-pointer"
+            className="text-sm p-3 hover:bg-sky-400 hover:text-white rounded-md transition-all cursor-pointer"
           >
             {item.text}
           </li>
         ))}
-        <button className="bg-yellow-300 px-4 py-2 rounded-full font-semibold hover:bg-yellow-400 transition">
+        <button className="bg-yellow-300 px-4 py-1 rounded-full font-semibold hover:bg-yellow-400 transition">
           Register
         </button>
       </ul>
@@ -47,7 +51,7 @@ const Navbar = () => {
       {/*Dark and light theme toogle*/}
 
       <div>
-        <button onClick={toggleTheme} className="text-2xl ">
+        <button onClick={toggleTheme} className="pl-25 pt-2 text-2xl ">
           {theme === "light" ? (
             <FaMoon />
           ) : (
@@ -56,25 +60,25 @@ const Navbar = () => {
         </button>
       </div>
 
-      {/* Social Icons */}
-      <div className="hidden xl:flex items-center gap-5 text-2xl text-gray-600">
-        <FaFacebook className="hover:text-blue-500 transition" />
-        <FaInstagram className="hover:text-pink-500 transition" />
-        <FaTiktok className="hover:text-black transition" />
-        <FaLinkedin className="hover:text-blue-700 transition" />
-      </div>
-
       {/* Mobile Menu Button */}
       <div className="xl:hidden block text-5xl cursor-pointer">
-        <IoMenu onClick={() => setIsMeanuOpen(!isMeanuOpen)} />
+        <IoMenu
+          onClick={() => {
+            console.log("Menu icon clicked!");
+            setIsMeanuOpen(!isMeanuOpen);
+          }}
+        />
       </div>
 
       {/* Mobile Menu */}
       <div
-        className={`absolute xl:hidden top-15 w-full right-0 bg-white flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform ${
-          isMeanuOpen ? "opacity-100" : "opacity-0"
-        }`}
-        style={{ transition: "transform 0.3s ease, opacity 0.3s ease" }}
+        className={`absolute xl:hidden top-16 w-full right-0 bg-white flex flex-col items-center gap-6 font-semibold text-lg
+    transform transition-transform duration-300 ease-in-out
+    ${
+      isMeanuOpen
+        ? "opacity-100 pointer-events-auto translate-y-0 z-50 "
+        : "opacity-0 pointer-events-none -translate-y-full"
+    }`}
       >
         <ul className="flex flex-col items-center gap-4">
           {NavLink.map((item, index) => (
